5ddd69c63e68518e544fc7dd601cc86672d3ecc0
Damian Federico <damianfederico@protonmail.com>
2020-07-09 21:10:25 -0300

    Add camera and car scripts

---
+++ Scripts/Car.gd
@@ -0,0 +1,44 @@
+extends VehicleBody
+
+export var MAX_ENGINE_FORCE = 200.0
+export var MAX_BRAKE_FORCE = 5.0
+export var MAX_STEER_ANGLE = 0.5
+
+export var steer_speed = 5.0
+
+var steer_target = 0.0
+var steer_angle = 0.0
+
+export var steering_mult = -1.0
+export var throttle_mult = 1.0
+export var brake_mult = 1.0
+
+
+func _physics_process(delta):
+	var steer_val = 0
+	var throttle_val = 0
+	var brake_val = 0
+
+	if Input.is_action_pressed("ui_up"):
+		throttle_val = 1.0
+	if Input.is_action_pressed("ui_down"):
+		brake_val = 1.0
+	if Input.is_action_pressed("ui_left"):
+		steer_val = 1.0
+	elif Input.is_action_pressed("ui_right"):
+		steer_val = -1.0
+	
+	engine_force = throttle_val * MAX_ENGINE_FORCE
+	brake = brake_val * MAX_BRAKE_FORCE
+	
+	steer_target = steer_val * MAX_STEER_ANGLE
+	if (steer_target < steer_angle):
+		steer_angle -= steer_speed * delta
+		if (steer_target > steer_angle):
+			steer_angle = steer_target
+	elif (steer_target > steer_angle):
+		steer_angle += steer_speed * delta
+		if (steer_target < steer_angle):
+			steer_angle = steer_target
+	
+	steering = steer_angle
