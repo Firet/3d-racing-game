f7a6c8c4a41a90f10e4729bc64ee706249bcd085
Damian Federico <damianfederico@protonmail.com>
2020-07-14 21:45:39 -0300

    Change variable names in raceFutureCar.gd

--- Scripts/raceFutureCar.gd
+++ Scripts/raceFutureCar.gd
@@ -1,42 +1,42 @@
 extends VehicleBody
 
-export var fuerza : float = 0
-export var frenos = 0
-export var giro = 0
+export var strength : float = 0
+export var brakes = 0
+export var steer = 0
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
 	# period character is similar to self
-	get_node(".").engine_force = fuerza
-	get_node(".").steering = giro
-	get_node(".").brake = frenos
+	get_node(".").engine_force = strength
+	get_node(".").steering = steer
+	get_node(".").brake = brakes
 	pass
 	
-	# Movimientos
+	# Movement
 	if Input.is_action_pressed("move_forward"):
 		
 		# 120 is magic number. max velocity 
-		if (fuerza < 120):
-			fuerza += 20
-			print(fuerza)
+		if (strength < 120):
+			strength += 20
+			print(strength)
 		else:
-			fuerza = 0
+			strength = 0
 			
 	if Input.is_action_pressed("move_backward"):
 		# 0.5 is magic number, increase to 1 in 0.5 
-		frenos += 0.5
+		brakes += 0.5
 		print('break')
 	else: 
-		frenos = 0
+		brakes = 0
 		
 	if Input.is_action_pressed("move_left"):
-		giro = 45
+		steer = 45
 		print('left')
 	elif Input.is_action_pressed("move_right"):
-		giro = -45
+		steer = -45
 		print('right')
 	else:
-		giro = 0
+		steer = 0
 
-# is similar to phtsic process
+# Is similar to phtsic process
 	#func _input(event):
