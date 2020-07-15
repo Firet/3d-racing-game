d1be39af29fab22e4ec7bcb0ea09cc268d51ea75
Damian Federico <damianfederico@protonmail.com>
2020-07-15 18:18:21 -0300

    Removes magic numbers in raceFuturCar.gd

--- Scripts/raceFutureCar.gd
+++ Scripts/raceFutureCar.gd
@@ -3,6 +3,9 @@ extends VehicleBody
 export var strength : float = 0
 export var brakes = 0
 export var steer = 0
+export var MAX_VELOCITY = 320
+# Brakes in ranges of 0.5 to 1
+export var STOPPING_POWER = 0.5
 
 # warning-ignore:unused_argument
 func _physics_process(delta):
@@ -15,15 +18,13 @@ func _physics_process(delta):
 	# Movement
 	if Input.is_action_pressed("move_forward"):
 		
-		# 120 is magic number. max velocity 
-		if (strength < 120):
+		if (strength < MAX_VELOCITY):
 			strength += 20
 			print(strength)
 		else:
 			strength = 0
 			
-	if Input.is_action_pressed("move_backward"):
-		# 0.5 is magic number, increase to 1 in 0.5 
+	if Input.is_action_pressed("move_backward"): 
 		brakes += 0.5
 		print('break')
 	else: 
