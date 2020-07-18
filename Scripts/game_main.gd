31817ee6af0d3cc40fd095892d297a04c806f058
Damian Federico <damianfederico@protonmail.com>
2020-07-18 00:54:00 -0300

    Fix camera rotation when release right click button

--- Scripts/game_main.gd
+++ Scripts/game_main.gd
@@ -4,11 +4,9 @@ var right_click = false
 var decreament = 0
 
 func _input(event):
-	#print('event')
 	if(event is InputEventMouseButton):
 		# Access to mouse button
 		if(event.button_index == BUTTON_RIGHT):
-			#print('event.button == button right')
 			if(event.pressed):
 				right_click = true
 				#print('right click pressed')
@@ -17,8 +15,8 @@ func _input(event):
 				#print('right click released')
 				
 	if(right_click && event is InputEventMouseMotion):
-		print('right_click and event mouse motion')
-	# We access to camera reference to move it
+		#print('right_click and event mouse motion')
+		# We access to camera reference to move it
 		decreament = 1
 		# Reference is an array. We access the first one
 		# Rotate is a camera property
@@ -26,8 +24,8 @@ func _input(event):
 		# If we want to move two axis to the same time we have to use Quaternion
 		get_tree().get_nodes_in_group("reference")[0].rotate(Vector3(0, - decreament, 0), event.speed.x / 1000)
 		# Avoid to rotate z axis
-		#get_tree().get_node_in_group("reference")[0].rotation_degrees.z = 0
+		get_tree().get_nodes_in_group("reference")[0].rotation_degrees.z = 0
 	elif(right_click == false && decreament > 0):
-		get_tree().get_nodes_in_group("reference")[0].rotation_degrees.y = 0
+		get_tree().get_nodes_in_group("reference")[0].rotation_degrees.y = 180
 	elif(right_click == false && decreament < 0):
-		get_tree().get_nodes_in_group("reference")[0].rotation_degrees.y = 0
+		get_tree().get_nodes_in_group("reference")[0].rotation_degrees.y = 180
