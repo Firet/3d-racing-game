0353e0b059ee178f96aae473393e2e5a7539c1b6
Damian Federico <damianfederico@protonmail.com>
2020-07-19 15:56:43 -0300

    Add zoom functionality

--- Scripts/game_main.gd
+++ Scripts/game_main.gd
@@ -3,29 +3,41 @@ extends Node
 var right_click = false
 var decreament = 0
 
+# If zoom(fov) value is higher than 179 and less than 1 throws error
+# Max and min values are set for convenience
+const MAX_ZOOM = 110
+const MIN_ZOOM = 40
+onready var camera = get_tree().get_nodes_in_group("camera_reference")[0].get_node("camera")
+
+
 func _input(event):
 	if(event is InputEventMouseButton):
-		# Access to mouse button
+		if event.pressed:
+			match event.button_index:
+				BUTTON_WHEEL_UP:
+					if(camera.fov > MIN_ZOOM):
+						camera.fov -= 5
+				BUTTON_WHEEL_DOWN:
+					if(camera.fov < MAX_ZOOM):
+						camera.fov += 5
 		if(event.button_index == BUTTON_RIGHT):
 			if(event.pressed):
 				right_click = true
-				#print('right click pressed')
 			else:
 				right_click = false
-				#print('right click released')
+				
 				
 	if(right_click && event is InputEventMouseMotion):
-		#print('right_click and event mouse motion')
 		# We access to camera reference to move it
 		decreament = 1
 		# Reference is an array. We access the first one
 		# Rotate is a camera property
 		# negative decre to rotate in y axis
 		# If we want to move two axis to the same time we have to use Quaternion
-		get_tree().get_nodes_in_group("reference")[0].rotate(Vector3(0, - decreament, 0), event.speed.x / 1000)
+		get_tree().get_nodes_in_group("camera_reference")[0].rotate(Vector3(0, - decreament, 0), event.speed.x / 1000)
 		# Avoid to rotate z axis
-		get_tree().get_nodes_in_group("reference")[0].rotation_degrees.z = 0
+		get_tree().get_nodes_in_group("camera_reference")[0].rotation_degrees.z = 0
 	elif(right_click == false && decreament > 0):
-		get_tree().get_nodes_in_group("reference")[0].rotation_degrees.y = 180
+		get_tree().get_nodes_in_group("camera_reference")[0].rotation_degrees.y = 180
 	elif(right_click == false && decreament < 0):
-		get_tree().get_nodes_in_group("reference")[0].rotation_degrees.y = 180
+		get_tree().get_nodes_in_group("camera_reference")[0].rotation_degrees.y = 180
