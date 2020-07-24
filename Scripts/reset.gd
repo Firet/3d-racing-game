8fbcaee61b2167790bb2e6c70dc3fef233223850
Damian Federico <damianfederico@protonmail.com>
2020-07-23 21:29:37 -0300

    Refactor reset.gd + Rename ResetButton node in first level

--- Scripts/reset.gd
+++ Scripts/reset.gd
@@ -6,14 +6,14 @@ func _physics_process(delta):
 
 # Reset level by clicking the UI button
 func _on_Button_pressed():
-	# warning-ignore:return_value_discarded
-	#get_tree().reload_current_scene()
-	#get_tree().change_scne("res://Level/Level1.tscn")
-	GameMain.laps = 0
+	reset()
 
 # Reset level by pressing the reset keyboard key
 func resetByPressingResetKey():
 	if Input.is_action_pressed("reset"):
-# warning-ignore:return_value_discarded
-		get_tree().reload_current_scene()
-		GameMain.laps = 0
+		reset()
+
+func reset():
+	# warning-ignore:return_value_discarded
+	get_tree().reload_current_scene()
+	GameMain.laps = 0
