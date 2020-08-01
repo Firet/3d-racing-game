4972407b4b80786babbf8eae0a20bcaca46da9aa
Damian Federico <damianfederico@protonmail.com>
2020-08-01 18:54:46 -0300

    Avoid counting laps when race is over

--- Scripts/finish_lap_area.gd
+++ Scripts/finish_lap_area.gd
@@ -2,7 +2,7 @@ extends Area
 
 
 func _on_Area_body_entered(body):
-	if(body.is_in_group("player")):
+	if(body.is_in_group("player") && GameMain.currentLap < GameMain.totalLaps):
 		GameMain.currentLap += 1
 		get_tree().get_nodes_in_group("laps")[0].text = "Lap " + String(GameMain.currentLap) + "/" + String(GameMain.totalLaps)
 		if(GameMain.currentLap >= GameMain.totalLaps):
